caa58c640612749754721a3a590d64d828fcacca
BLACK_ROSE <33515363+BL4CKR0S33@users.noreply.github.com>
2021-02-18 12:36:38 +0000

    Install the tool

--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@ if [ -d "/usr/share/octonum" ] ;
 then
 	echo "[!] Tool already installed, type octonum in your terminal."
 else
-	echo "[+] Installing packages ..."
+	echo -n "[+] Installing packages ... "
 	pip3 install phonenumbers > /dev/null
+	echo "[OK]"
 	sudo cp -r "." "/usr/share/octonum";
 	sudo echo -e "#!/bin/bash
 	python3 /usr/share/octonum/octonum.py" '${1+"$@"}' > "octonum";
 	sudo cp "octonum" "/usr/local/bin/octonum";
 	sudo chmod +x "/usr/local/bin/octonum";
 	rm "octonum";
-	echo "installation completed"
+	echo "[+] Installation completed"
 fi
